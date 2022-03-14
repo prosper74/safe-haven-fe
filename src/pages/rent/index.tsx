87f453908fe5cde7aedfe0ba9b3bda0ffe1e11aa
Prosper Atu <atu.prosper@gmail.com>
2022-03-14 16:08:38 +0100

    Added infinite scroll to the Rent Page

--- src/pages/rent/index.tsx
+++ src/pages/rent/index.tsx
@@ -1,15 +1,33 @@
 // index.tsx
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import Head from 'next/head';
 import PropertyCard from '@src/components/common/properties/propertyCard';
 import axios from 'axios';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import { singleProperties } from '@src/components/common/interfaces';
 
 interface IProps {
   properties: singleProperties;
+  totalCount: number;
 }
 
-const RentPage: FC<IProps> = ({ properties }) => {
+const RentPage: FC<IProps> = ({ properties, totalCount }) => {
+  const [isProperties, setIsProperties] = useState(properties);
+  const [hasMore, setHasMore] = useState(true);
+
+  const getMoreProperties = async () => {
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_REST_API}/properties?category.name=Rent&_start=${isProperties.length}&_limit=9&_sort=createdAt:desc`
+    );
+    const newProperties = await res.data;
+    // @ts-ignore
+    setIsProperties((isProperties) => [...isProperties, ...newProperties]);
+  };
+
+  useEffect(() => {
+    setHasMore(totalCount > isProperties.length! ? true : false);
+  }, [isProperties]);
+
   return (
     <>
       <Head>
@@ -21,13 +39,34 @@ const RentPage: FC<IProps> = ({ properties }) => {
       <main className="mt-24">
         <div className="container xs:px-0 md:px-6 xl:px-32 mx-auto bg-white">
           <h1 className="font-bold text-center text-3xl mt-32 mb-16">
-            Rent Ads({properties.length})
+            Rent Ads({totalCount})
           </h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 2xl:gap-1 mb-32">
-            {properties.map((property: singleProperties) => (
-              <PropertyCard key={property.id} property={property} />
-            ))}
-          </div>
+          <InfiniteScroll
+            // @ts-ignore
+            dataLength={isProperties.length}
+            next={getMoreProperties}
+            hasMore={hasMore}
+            loader={
+              <div className="flex items-center justify-center mt-6 mb-32">
+                <img
+                  src="https://res.cloudinary.com/prosper-dev/image/upload/v1647127499/eclipse_qljx8s.svg"
+                  alt="Loader"
+                  className="m-auto w-12"
+                />
+              </div>
+            }
+            endMessage={
+              <p className="text-center mt-6 mb-32">
+                <b>No more properties to load</b>
+              </p>
+            }
+          >
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 2xl:gap-1 mb-32">
+              {properties.map((property: singleProperties) => (
+                <PropertyCard key={property.id} property={property} />
+              ))}
+            </div>
+          </InfiniteScroll>
         </div>
       </main>
     </>
@@ -38,11 +77,16 @@ export default RentPage;
 
 export async function getServerSideProps() {
   const res = await axios.get(
-    `${process.env.NEXT_PUBLIC_REST_API}/properties?category.name=Rent&_sort=createdAt:desc`
+    `${process.env.NEXT_PUBLIC_REST_API}/properties?category.name=Rent&_limit=9&_sort=createdAt:desc`
   );
+  const totalCount = await axios.get(
+    `${process.env.NEXT_PUBLIC_REST_API}/properties/count?category.name=Rent`
+  );
+
   return {
     props: {
       properties: res.data,
+      totalCount: totalCount.data,
     },
   };
 }
