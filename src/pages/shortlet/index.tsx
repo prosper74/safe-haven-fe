e2e0868a17c3534b5466f15c64170abc6f706305
Prosper Atu <atu.prosper@gmail.com>
2022-03-14 16:19:12 +0100

    Modified the styles on Shortle and Rent Pages for the infinite scroll component

--- src/pages/shortlet/index.tsx
+++ src/pages/shortlet/index.tsx
@@ -61,7 +61,7 @@ const ShortletPage: FC<IProps> = ({ properties, totalCount }) => {
               </p>
             }
           >
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 2xl:gap-1 mb-32">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 2xl:gap-1">
               {properties.map((property: singleProperties) => (
                 <PropertyCard key={property.id} property={property} />
               ))}
