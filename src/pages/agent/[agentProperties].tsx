e93c4a6b18286eed44f9b770dc4a85e07c6433d7
Prosper Atu <atu.prosper@gmail.com>
2022-03-17 22:01:35 +0100

    Little more testing on  mobile screen

--- src/pages/agent/[agentProperties].tsx
+++ src/pages/agent/[agentProperties].tsx
@@ -26,7 +26,7 @@ const BuySingle: FC<IProps> = ({ properties, agent }) => {
       </Head>
 
       <main className="my-24">
-        <div className="container xs:px-0 md:px-6 xl:px-32 mx-auto bg-white">
+        <div className="sm:container xs:px-4 md:px-6 xl:px-32 mx-auto bg-white">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-0 sm:gap-4 mt-6">
             {/* agent sidebar */}
             <div>
