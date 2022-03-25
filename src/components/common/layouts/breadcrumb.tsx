71a0a41972a4e2bfdfc69be18129a7ae3f878bac
Prosper Atu <atu.prosper@gmail.com>
2022-03-25 09:35:48 +0100

    Moved the Breadcrumb SVG to the SVG component

--- src/components/common/layouts/breadcrumb.tsx
+++ src/components/common/layouts/breadcrumb.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import Link from 'next/link';
+import { HomeAltIcon } from '../svgIcons';
 
 interface IProps {
   property: string;
@@ -13,14 +14,8 @@ const Breadcrumb: FC<IProps> = ({ property, category }) => {
         <li className="inline-flex items-center">
           <Link href="/">
             <a className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-purple-600 dark:text-gray-400 dark:hover:text-white">
-              <svg
-                className="mr-2 w-4 h-4"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
-              </svg>
+              <HomeAltIcon />
+              
               Home
             </a>
           </Link>
